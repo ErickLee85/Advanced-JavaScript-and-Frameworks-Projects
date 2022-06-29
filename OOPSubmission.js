23c4055009a9e4f3e486530ee649a24c5631d618
ErickC <ErickCrowne@outlook.com>
2022-06-29 10:59:10 -0500

    class instantiation assignment.

---
+++ OOPSubmission.js
@@ -0,0 +1,20 @@
+class Dog {
+    constructor(Breed, Color, Height, Weight) {
+        this.Breed = Breed;
+        this.Color = Color;
+        this.Height = Height;
+        this.Weight = Weight;
+    }
+    Shake () {
+       //Code to be executed;
+    }
+    Sit () {
+        //Code to be executed;
+  }
+    LayDown() {
+        //Code to be executed;
+           }
+}
+
+var myPuppy = new Dog("Hound", "Brown", "2 Feet", "100lbs");
+console.log(myPuppy);
