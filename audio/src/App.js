cc4a86f018060da1eae3bf4fd08ac81a59ad5738
ErickC <ErickCrowne@outlook.com>
2022-08-11 14:18:13 -0500

    commit

--- audio/src/App.js
+++ audio/src/App.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react"
+import Sound2 from "./song/backgroundMusic.mp3"
+import { Howl } from 'howler'
 
+ const sound = new Howl({
+  src: [Sound2],
+ });
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default class extends Component {
-  playAudio() {
-    const audioEl = document.getElementsByClassName("audio-element")[0]
-    audioEl.play()
+  PlayAudio = () => {
+   if (sound.playing() === false) { //using this conditional to make sure sound doesnt repeat on top of its self
+      sound.play()
+   }
+  }
+
+  PauseAudio = () => {
+    sound.pause()
+  }
+
+  RenderButtonAndSound = () => {
+      return (
+       <div>
+         <button onClick={() => this.PlayAudio()}>
+          Play Music
+        </button>
+        
+        <button onClick={() => this.PauseAudio()}>
+          Pause Music
+        </button>
+       </div>
+      )
   }
 
   render() {
     return (
-      <div>
-        <button onClick={this.playAudio}>
-          <span>Play Audio</span>
-        </button>
-        <audio className="audio-element">
-          <source src="./song/beep.mp3"></source>
-        </audio>
+      <div className="App">
+        {this.RenderButtonAndSound()}
       </div>
     )
   }
